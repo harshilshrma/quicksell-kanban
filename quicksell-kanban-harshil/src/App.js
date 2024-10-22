36a1b15295f0268fb53a090812d17160e6a4fc26
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-10-22 16:51:04 +0530

    basic app structure done

---
+++ quicksell-kanban-harshil/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kanban Board</h1>
+      <p>Let's build this step by step!</p>
+    </div>
+  );
+}
+
+export default App;
